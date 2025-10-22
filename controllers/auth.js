02702a178956a717d68ae78dcace14342dc42e2c
haibn <haibinyuy@berkeley.edu>
2025-10-21 22:04:44 -0700

    backend finished

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,44 +1,56 @@
-const User = require('../models/User')
-const {StatusCodes} = require('http-status-codes')
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
-const { BadRequestError, UnauthenticatedError } = require('../errors')
+const User = require("../models/User");
+const { StatusCodes } = require("http-status-codes");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { BadRequestError, UnauthenticatedError } = require("../errors");
 
 const register = async (req, res) => {
-    const user = await User.create({...req.body})
-    const token = user.createJWT()
-    res.status(StatusCodes.CREATED).json({user: {name: user.name}, token});
-}
+  const user = await User.create({ ...req.body });
+  const token = user.createJWT();
+  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
+};
 
 const login = async (req, res) => {
-    const {email, password} = req.body
+  const { email, password } = req.body;
 
-    if (!email || !password) {
-        throw new BadRequestError('Please provide valid email and password')
-    }
+  if (!email || !password) {
+    throw new BadRequestError("Please provide valid email and password");
+  }
 
-    const user = await User.findOne({email})
-    if (!user) {
-        throw new UnauthenticatedError('Invalid Credentials')
-    }
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new UnauthenticatedError("Invalid Credentials");
+  }
 
-    // compare password
-    const isPasswordCorrect = await user.comparePassword(password)
-    if (!isPasswordCorrect) {
-        throw new UnauthenticatedError('Invalid Credentials')
-    }
+  // compare password
+  const isPasswordCorrect = await user.comparePassword(password);
+  if (!isPasswordCorrect) {
+    throw new UnauthenticatedError("Invalid Credentials");
+  }
 
-    const token = user.createJWT()
+  const token = user.createJWT();
 
-    res.status(StatusCodes.OK).json({user: {name: user.name}, token})
-}
+  res.status(StatusCodes.OK).json({ user: { name: user.name }, token });
+};
 
 const getCurrentUser = async (req, res) => {
-    res.send('Current user details');
-}
+  const user = await User.findById(req.user.userId).select("-password");
+  if (!user) {
+    throw new UnauthenticatedError("User not found");
+  }
+
+  res.status(StatusCodes.OK).json({
+    message: "Current user fetched successfully",
+    user: {
+      id: user._id,
+      name: user.name,
+      email: user.email,
+    },
+  });
+};
 
 module.exports = {
-    register,
-    login,
-    getCurrentUser
+  register,
+  login,
+  getCurrentUser,
 };
