2b0b23913dffe1f23afe3b9da1df9fd58833a651
haibn <haibinyuy@berkeley.edu>
2025-10-23 17:53:14 -0700

    service-account

--- googleSheetsService.js
+++ googleSheetsService.js
@@ -1,14 +1,33 @@
 const { google } = require("googleapis");
-const path = require("path");
 
-const auth = new google.auth.GoogleAuth({
-  keyFile: path.join(__dirname, "service-account.json"),
-  scopes: ["https://www.googleapis.com/auth/spreadsheets"],
-});
+const getAuth = () => {
+  if (!process.env.GOOGLE_SERVICE_ACCOUNT_KEY) {
+    throw new Error("Missing GOOGLE_SERVICE_ACCOUNT_KEY environment variable");
+  }
 
-const sheets = google.sheets({ version: "v4", auth });
+  // Parse the JSON key from environment variable
+  const serviceAccount = JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT_KEY);
 
+  // Create authentication object
+  const auth = new google.auth.GoogleAuth({
+    credentials: {
+      client_email: serviceAccount.client_email,
+      private_key: serviceAccount.private_key.replace(/\\n/g, "\n"), // handle escaped newlines
+    },
+    scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+  });
+
+  return auth;
+};
+
+const getSheetsClient = () => {
+  const auth = getAuth();
+  return google.sheets({ version: "v4", auth });
+};
+
+// Read data from sheet
 const readSheet = async (spreadsheetId, range) => {
+  const sheets = getSheetsClient();
   const response = await sheets.spreadsheets.values.get({
     spreadsheetId,
     range,
@@ -16,7 +35,9 @@ const readSheet = async (spreadsheetId, range) => {
   return response.data.values;
 };
 
+// Write data to sheet
 const writeSheet = async (spreadsheetId, range, values) => {
+  const sheets = getSheetsClient();
   const response = await sheets.spreadsheets.values.update({
     spreadsheetId,
     range,
